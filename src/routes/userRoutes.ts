ec3a9a7c9f49de7a8c1e45fd28c907bd39e20f0a
roberto <betomax@gmail.com>
2019-10-23 01:34:31 -0500

    mensaje en profile 4

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -52,7 +52,7 @@ class UserRoutes {
                 return res.json({message:'Sin Rol'});
                 break;
         }
-        return res.json(result);
+        return res.json({result, idUsuario});
         result[0].password = undefined;
         res.json(result[0]);
     }
